3b6e5f58a0d710fed747c99bd9a7a97acc2e993e
Vipper <vipper1989@gmail.com>
2013-09-06 11:08:10 -0300

    Trying use route

--- story.js
+++ story.js
@@ -1,38 +1,79 @@
-var myApp = angular.module("myApp", []);
+var myApp = angular.module('ngView', [], function($routeProvider, $locationProvider) {
+  $routeProvider.when('/Story', {
+    templateUrl: 'story.html',
+    controller: StoryCtrl
+  });
+  $routeProvider.when('/Stage', {
+    templateUrl: 'stage.html',
+    controller: StageCtrl
+  });
+ 
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(false);
+});
+ 
+function MainCntl($scope, $route, $routeParams, $location) {
+  $scope.$route = $route;
+  $scope.$location = $location;
+  $scope.$routeParams = $routeParams;
+  console.log($route);
+  console.log($location);
+  console.log($routeParams);
+}
 
 myApp.factory('Stages', function () {
   return [
-    {name: "backlog", id: 1},
-    {name: "in progress", id: 2},
-    {name: "done", id: 3},
+    "backlog",
+    "in progress",
+    "done"
   ];
 })
 
-function StoryCtrl($scope, Stages) {
+function StoryCtrl($scope, Stages, $routeParams) {
   $scope.stages = Stages;
+  $scope.params = $routeParams;
+  console.log($routeParams)
 
   $scope.stories = [
-    {title: "first story", description: "description", points: 4, stage_id: 1},
-    {title: "second story", description: "description", points: 2, stage_id: 1},
-    {title: "story in progress", description: "description", points: 2, stage_id: 2},
-    {title: "story done", description: "description", points: 2, stage_id: 3}
+    {title: "first story", description: "description", points: 4, stage: "backlog"},
+    {title: "second story", description: "description", points: 2, stage: "backlog"},
+    {title: "story in progress", description: "description", points: 2, stage: "in progress"},
+    {title: "story done", description: "description", points: 2, stage: "done"}
   ];
   
   $scope.addstory = function () {
-    $scope.newstory.stage_id = 1;
+    $scope.newstory.stage = $scope.stages[0];
     $scope.stories.push($scope.newstory);
     $scope.newstory = {}
   };
 
   $scope.move = function (story) {
-    story.stage_id++;
+    var oldIndex = $scope.stages.indexOf(story.stage);
+    console.log(story);
+    story.stage = $scope.stages[oldIndex+1];
+    console.log(story);
   };
 }
-function StageCtrl($scope, Stages) {
+function StageCtrl($scope, Stages, $routeParams) {
   $scope.stages = Stages;
+  $scope.params = $routeParams;
 
   $scope.addStage = function () {
     $scope.stages.push({name:$scope.stagename, id:$scope.stages.length + 1});
+  }
+
+  $scope.up = function(index)
+  { 
+    var tmp = $scope.stages[index];
+    $scope.stages[index] = $scope.stages[index-1];
+    $scope.stages[index-1] = tmp;
+  }
+
+  $scope.down = function(index)
+  { 
+    var tmp = $scope.stages[index];
+    $scope.stages[index] = $scope.stages[index+1];
+    $scope.stages[index+1] = tmp;
   }
   
 }
